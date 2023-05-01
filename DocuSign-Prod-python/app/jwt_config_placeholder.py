1c9502caf2bcad7deb1f6a56a1d49daa7b094e24
Matt <mattoneill215@gmail.com>
2023-05-01 16:44:51 -0400

    Added docusign system files

---
+++ DocuSign-Prod-python/app/jwt_config_placeholder.py
@@ -0,0 +1,10 @@
+DS_JWT = {
+    "ds_client_id": "",
+    "ds_impersonated_user_id": "",  # The id of the user.
+    "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
+    "authorization_server": "account-d.docusign.com",
+    "doc_docx": "Application.docx",
+    "doc_pdf": "Application.pdf",
+    "cc_email": "placeholder@UpwardBound.com",
+    "cc_name": "John Doe"
+}
